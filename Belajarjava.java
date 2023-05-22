de72b16bb6da97f6385fa097bcf129050223cfb6
Erik Fajar <erikfk1305@gmail.com>
2023-05-22 20:26:02 +0700

    Java Package

---
+++ Belajarjava.java
@@ -0,0 +1,10 @@
+import duniailkom.Komputer;
+   
+class BelajarJava {
+  public static void main(String args[]){ 
+   
+    Komputer KomputerAndi = new Komputer();
+    System.out.println(KomputerAndi.cekInfo());
+ 
+  }
+}
---
+++ duniailkom/Komputer.java
@@ -0,0 +1,7 @@
+package duniailkom;
+ 
+public class Komputer {
+   public String cekInfo() { 
+    return "Perkenalkan nama saya ERIK XI RPL"; 
+  }
+}
